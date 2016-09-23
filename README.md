9cd163dbb54b9ba38ecec7e8a82fa72deab28bc8
Tengfei Yang <hzyangtengfei@corp.netease.com>
2016-09-23 15:07:45 +0800

    java quick start template

---
+++ README.md
@@ -0,0 +1,6 @@
+# Spring Boot Application: webapp templcate
+
+## 常用命令
+
+- `mvn spring-boot:run` : 运行应用
+- `mvn package` : 生成可执行的 `jar` 文件
---
+++ {{cookiecutter.app_name}}/src/main/java/{{cookiecutter.app_name}}/Main.java
@@ -0,0 +1,8 @@
+package {{cookiecutter.app_name}};
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello {{cookiecutter.app_name}}");
+    }
+}
